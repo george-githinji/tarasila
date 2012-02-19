9cd694995fb8b334e27c3dea53c3919ef812627d
George Githinji <georgkam@gmail.com>
2012-02-20 01:09:46 +0300

    reading a fasta record

--- read_fasta.d
+++ read_fasta.d
@@ -1,7 +1,7 @@
 
-/* read the contents of a fasta file from std input
+/* read the contents of a file from std input
    copyright (c) 2012
-   learning D programming
+   Learning D programming for bioinformatics
 
  */
 
@@ -11,34 +11,47 @@ import std.stream;
 import std.regex;
 import std.array;
 import std.algorithm;
+import std.string;
 import bio.sequence.fasta;
-
 
 int main(string[] args){
   auto filename = args[1];
+  auto entry_name = regex(r"^>(.*)");
+  //auto fasta_regex = regex(r"(\>.+\n)([^\>]+\n)");
+
+  Stream file = new BufferedFile(filename);
   auto current = appender!(char[]);
   string name;
   string[string] map;
-
-  Stream file = new BufferedFile(filename);
-  auto entry_name = regex(r"^>(.*)");
-
-  foreach(ulong n, char[] line; file) {
+  
+  foreach(ulong n,char[] line; file) {
     auto entry = match(line,entry_name);
     if(entry){//we are in a header line
       if(name){//write what was caught 
         map[name] = current.data.idup;//dup because .current.data is reused
       }
-      name = entry.hit.idup[1..$].chomp; //remove the ">" 
+      name = entry.hit.idup[1..$].chomp; //also get rid of the ">" character
       current.clear();
     }else{
       //remove spaces from the line
+      //string line1 = line.idup;
+      //char[] line2 = removechars(line1," ").dup;
       line = removechars(line," ".dup);
+      //make characters lowercase
+      line = toLower(line);
       current.put(line);
     }
   }
   map[name] = current.data.idup;//remember last capture
   file.close();
   writeln(map);
+
+  entry ent = new entry();
+  ent.name = "seq1";
+  ent.seq  = "acgcccgat";
+
+  writeln(ent.get_name);
+  writeln(ent.get_seq);
+  writeln(ent.count("A"));
   return 0;
 }
